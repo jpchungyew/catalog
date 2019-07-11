46f5187567e90f56740b283bedf9acdc250e584c
Jean-Paul Chung-Yew <jp.chungyew@gmail.com>
2019-07-11 06:27:48 -0600

    Created routes

--- database_setup.py
+++ database_setup.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -27,10 +27,8 @@ class User(Base):
 class Category(Base):
     __tablename__ = 'category'
    
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    user_id = Column(Integer,ForeignKey('user.id'))
-    user = relationship(User)
+    id = Column(Integer, autoincrement=True)
+    name = Column(String(250), primary_key=True)
 
     @property
     def serialize(self):
@@ -42,8 +40,8 @@ class Category(Base):
 class Item(Base):
     __tablename__ = 'item'
 
-    id = Column(Integer, primary_key = True)
-    name =Column(String(80), nullable = False)
+    id = Column(Integer, autoincrement=True)
+    name =Column(String(80), primary_key=True)
     description = Column(String(250))
     category_id = Column(Integer,ForeignKey('category.id'))
     category = relationship(Category)
